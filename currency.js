95e02341c84a9227c7d8815d0c84f27cbc6d44a8
Kate <55151099+katekocalgary@users.noreply.github.com>
2019-10-28 21:24:00 -0600

    updated to simple way

--- currency.js
+++ currency.js
@@ -1,49 +1,35 @@
 
 // Define currency data
-const currencyData = [
-    { 
-        name : "CadToUsd",
-        rate : 1.5
-    },
-    { 
-        name : "UsdToCad",
-        rate : 0.5
-    }
-];
+const currencyData = { 
+        CadToUsd : 1.5,
+        UsdToCad : 0.5
+    };
 
 // CadToUsd module export
 exports.CadToUsd = function(amount) {
-    const cadToUseRate = currencyData.filter(function(x) {
-        return x.name === "CadToUsd";
-    }) 
-    return amount * cadToUseRate[0].rate;
-};
+    return amount * Number(currencyData.CadToUsd);
+    } ;
+
 
 // UsdToCad module export
 exports.UsdToCad = function(amount) { 
-    const UsdToCadRate = currencyData.filter(function(x){
-        return x.name === "UsdToCad";
-    })
-    return amount * UsdToCadRate[0].rate;
-};
+    return amount * Number(currencyData.UsdToCad);
+    } ;
 
 // New currency rate setting module export
 exports.setRate = function(rateName, rateValue) {
-    const newRateIndex = currencyData.findIndex(x => x.name === rateName);
-    if (newRateIndex != -1) {
-        currencyData[newRateIndex].rate = rateValue;
-        return true; 
- 
+    if (currencyData[rateName]) {
+        currencyData[rateName] = rateValue;
+        return true;
     } else {
         return false;
-    }    
+    } 
 }
 
 // Current currency rate getting module export
 exports.getRate = function(rateName){
-    const newRateIndex = currencyData.findIndex(x => x.name === rateName);
-    if (newRateIndex != -1){
-        return currencyData[newRateIndex].rate;
+    if (currencyData[rateName]){
+        return currencyData[rateName];
     }
     else {
         return undefined;
